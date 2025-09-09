72bef274ccf0892302e8f9e1446b704e93f17146
da2my <davo.da2m@gmail.com>
2025-09-09 11:21:18 +0200

    actualizacion de arbol para que sea mas escalable y que se ejecute con run.py

--- config.py
+++ config.py
@@ -1,17 +1,34 @@
 # config.py
 import os
+from dotenv import load_dotenv
+
+# Carga las variables del archivo .env en el entorno
+basedir = os.path.abspath(os.path.dirname(__file__))
+load_dotenv(os.path.join(basedir, '.env'))
 
 class Config:
     """Configuración base, compartida por todos los entornos."""
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'una-clave-secreta-por-defecto'
+    SECRET_KEY = os.environ.get('SECRET_KEY')
     DEBUG = False
     TESTING = False
 
 class DevelopmentConfig(Config):
     """Configuración para desarrollo."""
+    print("Cargando configuración de desarrollo")
     DEBUG = True
     # Ejemplo: DATABASE_URI = 'sqlite:///dev.db'
 
 class ProductionConfig(Config):
     """Configuración para producción."""
     # Ejemplo: DATABASE_URI = os.environ.get('DATABASE_URI')
+
+# class TestingConfig(Config):
+    # # ...    
+
+# Diccionario para mapear nombres a clases
+# config_by_name = {
+    # 'development': DevelopmentConfig,
+    # 'testing': TestingConfig,
+    # 'production': ProductionConfig,
+    # 'default': DevelopmentConfig
+# }    
---
+++ src/__init__.py
@@ -0,0 +1,18 @@
+# src/__init__.py
+from flask import Flask
+from config import Config
+
+def create_app(config_class=Config):
+    """
+    Función fábrica para crear la instancia de la aplicación Flask.
+    """
+    app = Flask(__name__)
+    app.config.from_object(config_class)
+
+    # Registrar el Blueprint desde el módulo 'main' necesaria en Flask para evitar importaciones circulares.
+    from .main.routes import main as main_blueprint
+    app.register_blueprint(main_blueprint)
+
+    # Aquí podrías registrar más Blueprints o extensiones
+
+    return app
