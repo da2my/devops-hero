762c967f2dd1e395a37d0be9e1222941f5c0edeb
da2my <davo.da2m@gmail.com>
2025-09-09 12:39:16 +0200

    codigo de anterior commit funciona, la identacion para flake8 es correcta. Actual proceso, escribir un test

---
+++ test/test_app.py
@@ -0,0 +1,29 @@
+import pytest
+from src import create_app
+from config import TestingConfig
+
+@pytest.fixture
+def client():
+    """
+    Crea un cliente de prueba para la aplicación Flask.
+    Esto es una 'fixture' de pytest.
+    """
+    # Crea una instancia de la app con la configuración de testing
+    app = create_app(TestingConfig)
+
+    # El 'test_client' permite simular peticiones a la app sin levantar un servidor
+    with app.test_client() as client:
+        yield client
+
+def test_hello_route(client):
+    """
+    GIVEN un cliente de prueba de Flask
+    WHEN se hace una petición GET a la ruta '/'
+    THEN se debe recibir una respuesta exitosa (código 200) y el contenido correcto
+    """
+    # Hacemos la petición a la ruta raíz
+    response = client.get('/')
+
+    # Verificamos que la respuesta sea la correcta
+    assert response.status_code == 200
+    assert b"DevOps test desde la nueva estructura!" in response.data
